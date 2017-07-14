0502b0c5e436e4332298712ee7b786fa497fdc52
Marc Egea i Sala <marc.egea.sala@gmail.com>
2017-07-14 16:05:25 +0000

    Add a default "help" task

--- lib/App/Please.pm
+++ lib/App/Please.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use File::Basename;
 
+use App::Please::Help;
 use App::Please::Root;
 use App::Please::RootFinder;
 
@@ -14,6 +15,10 @@ my $root = App::Please::Root->new($root_path);
 
 if (my $task = $root->get_task($task_name)) {
     system($task->path, @ARGV);
+}
+# We provide a default help task
+elsif ($task_name eq 'help') {
+    say App::Please::Help->new($root)->text;
 }
 else {
     die "$task_name is not a task";
